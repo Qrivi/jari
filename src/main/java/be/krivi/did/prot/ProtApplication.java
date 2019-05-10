79b480cbc33801a9abd23fc4305bf9f59ebcb0e8
Kristof Dewilde <git@krivi.be>
2019-05-10 15:10:27 +0200

    hello world

---
+++ src/main/java/be/krivi/did/prot/ProtApplication.java
@@ -0,0 +1,13 @@
+package be.krivi.did.prot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtApplication.class, args);
+	}
+
+}
