66a94dcbbe6cee5f49162acb520dfcbb3a43fff2
Kristof Dewilde <git@krivi.be>
2019-05-23 18:15:12 +0200

    rg

---
+++ src/main/java/be/krivi/did/prot/core/model/User.java
@@ -0,0 +1,39 @@
+package be.krivi.did.prot.core.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table( name = "user")
+public class User implements Serializable{
+
+    @Id
+    @GeneratedValue( generator = "pk_generator", strategy = GenerationType.TABLE )
+    @TableGenerator( name = "pk_generator", allocationSize = 1 )
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank( message = "{NotBlank.User.name}" )
+    @Size( max = 50, message = "{Size.User.name}" )
+    @Column( name = "name" )
+    private String name;
+
+    @NotBlank( message = "{NotBlank.User.slackId}" )
+    @Size( max = 8, message = "{Size.User.slackId}" )
+    @Column( name = "slack_id" )
+    private String slackId;
+
+}
