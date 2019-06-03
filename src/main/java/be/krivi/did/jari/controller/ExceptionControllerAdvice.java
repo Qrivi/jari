e6e0223dfb277e63402a705af18ba2e0cff2bf74
Kristof Dewilde <git@krivi.be>
2019-06-03 17:07:21 +0200

    Added ExceptionHandler for constraint violations and db transaction exceptions

--- src/main/java/be/krivi/did/jari/controller/ExceptionControllerAdvice.java
+++ src/main/java/be/krivi/did/jari/controller/ExceptionControllerAdvice.java
@@ -5,8 +5,13 @@ import be.krivi.did.jari.response.Response;
 import be.krivi.did.jari.service.MessageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice{
@@ -15,6 +20,25 @@ public class ExceptionControllerAdvice{
 
     public ExceptionControllerAdvice( MessageService messageService ){
         this.messageService = messageService;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Response> handleConstraintViolationException( ConstraintViolationException e ){
+        String messages = e.getConstraintViolations()
+                .stream()
+                .map( ConstraintViolation::getMessageTemplate )
+                .map( m -> m.replaceAll( "[{}]", "" ) )
+                .map( messageService::get )
+                .collect( Collectors.joining( ". ", "", "." ) );
+
+        return new ResponseEntity<>( Response.bad( messages ), HttpStatus.BAD_REQUEST );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Response> handleTransactionSystemException( TransactionSystemException e ){
+        if( e.getRootCause() instanceof ConstraintViolationException )
+            return handleConstraintViolationException( (ConstraintViolationException) e.getRootCause() );
+        return new ResponseEntity<>( Response.bad( e.getMessage() ), HttpStatus.INTERNAL_SERVER_ERROR );
     }
 
     @ExceptionHandler
